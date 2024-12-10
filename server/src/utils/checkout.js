29abb9a8f8294fe4e78ad0d3a7f9457313f67634
YagoAyala <yago.ayala@outlook.com>
2024-12-10 20:15:14 -0300

    feat: created checkout

---
+++ server/src/utils/checkout.js
@@ -0,0 +1,21 @@
+// const mercadopago = require("mercadopago");
+
+// mercadopago.configure({
+//   access_token: "TEST-6462854186063055-081316-c64068043c99699a87c1c0f06318df41-274053496"
+// });
+
+// const preference = {
+//   items: [
+//     {
+//       title: "Sistema LotoAI",
+//       unit_price: 19.90,
+//       quantity: 1,
+//     },
+//   ],
+// };
+
+// mercadopago.preferences.create(preference).then((response) => {
+//   console.log("response: ", response);
+// }).catch((error) => {
+//   console.log("error: ", error);
+// });
