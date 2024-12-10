d824fea50f7cb935d2a4ed5b6cd79530a971e67a
YagoAyala <yago.ayala@outlook.com>
2024-12-10 20:13:52 -0300

    feat: created utils

---
+++ server/src/utils/utils.js
@@ -0,0 +1,13 @@
+const checkIfIsPrime = require("./checkIfIsPrime");
+const isFibonacci = require("./checkIfIsFibonacci");
+const sumArrayNumbers = require("./sumArrayNumbers");
+const geneticAlgorithm = require("./geneticAlgorithm");
+const neuralNetwork = require("./neuralNetwork");
+
+module.exports = {
+	checkIfIsPrime,
+	isFibonacci,
+	sumArrayNumbers,
+	geneticAlgorithm,
+	neuralNetwork,
+};
